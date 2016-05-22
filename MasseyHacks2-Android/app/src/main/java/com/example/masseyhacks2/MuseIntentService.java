25d3436d6de04c9ef975dd1087ba6df65738c0df
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-05-22 04:32:15 -0400

    Create MuseIntentService for background operations

---
+++ MasseyHacks2-Android/app/src/main/java/com/example/masseyhacks2/MuseIntentService.java
@@ -0,0 +1,25 @@
+package com.example.masseyhacks2;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Bruno on 2016-05-22.
+ */
+public class MuseIntentService extends IntentService {
+
+    public static String museIntentKey = "muse";
+
+    public MuseIntentService() {
+        this("MuseIntentService");
+    }
+
+    public MuseIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
